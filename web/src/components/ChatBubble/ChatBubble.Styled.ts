34f6b617d65b4ca0b8972510566ab686475ea196
Tanel Tekko <taneltekko@Tanels-MacBook-Pro.local>
2022-06-18 17:29:34 +0300

    Chat component style

--- web/src/components/ChatBubble/ChatBubble.Styled.ts
+++ web/src/components/ChatBubble/ChatBubble.Styled.ts
@@ -7,32 +7,22 @@ interface Props {
 
 
 export const ChatBubbleContainer = styled.div<Props>`
-    border: 2px solid #dedede;
+   // border: 2px solid  ${(props) => props.rightSide == true ? '#4168A' : '#79E2E2'};
     background-color: #f1f1f1;
     border-radius: 10px;
     padding: 10px;
     margin: 10px 0;
-    text-align: ${(props) => props.rightSide === true ? 'left' : 'right'};
+    align-self: ${(props) => props.rightSide === true ? 'start' : 'end'};
     border: 4px solid #00bfb6;
     color: #00bfb6;
     font-family: arial;
     position: relative;
     font-size: 14px;
-    &:after {
-       content: ${props => !!props.rightSide ? 'content' : " 🦄"};
-        width: 0px;
-        height: 0px;
-        position: absolute;
-        border-left: 10px solid #00bfb6;
-        border-right: 10px solid transparent;
-        border-top: 10px solid #00bfb6;
-        border-bottom: 10px solid transparent;
-        right: -20px;
-        top: 6px;
-            }
+    width: max-content;
 `;
 
 export const Text = styled.p`
     color: #000;
+    margin: 0;
 `;
 
