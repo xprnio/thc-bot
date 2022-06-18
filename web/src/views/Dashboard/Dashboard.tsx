73be63e722048154bad01c9778df5260d4ee0cf2
Ragnar <ragnar@xprnio.xyz>
2022-06-18 21:30:00 +0300

    Clean up dashboard

--- web/src/views/Dashboard/Dashboard.tsx
+++ web/src/views/Dashboard/Dashboard.tsx
@@ -1,16 +1,18 @@
-import { AssetCard, ProfitCard } from "../../components/Card";
+import { AssetCard, ProfitCard } from '../../components/Card';
 import { Label } from '../../components/Label/Label';
-import { Scrollable } from "../../components/Scrollable";
-import { DashboardContainer } from "./Dashboard.styled";
+import { Scrollable } from '../../components/Scrollable';
+import { DashboardContainer } from './Dashboard.styled';
 
-const Dashboard = () =>
+const Dashboard = () => {
+  // TODO: Implement data
+  return (
     <DashboardContainer>
       <Label type="heading">Dashboard</Label>
-      <Label type="title">Total earnings</Label>
+      <Label type="title">Profit</Label>
       <Scrollable>
         <ProfitCard label="Total" profit={420} margin={69} />
-        <ProfitCard label="Total" profit={420} margin={69} />
-        <ProfitCard label="Total" profit={420} margin={69} />
+        <ProfitCard label="Today" profit={420} margin={69} />
+        <ProfitCard label="This week" profit={420} margin={69} />
       </Scrollable>
       <Label type="title">Assets</Label>
       <Scrollable>
@@ -19,6 +21,7 @@ const Dashboard = () =>
         <AssetCard label="Monero" profit={420} margin={69} total={1337} />
       </Scrollable>
     </DashboardContainer>
-;
+  );
+};
 
 export default Dashboard;
