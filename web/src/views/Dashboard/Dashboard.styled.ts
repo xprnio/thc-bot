c66dce4483cf06dc8a25ffc66486f6abdf981d73
Ragnar <ragnar@xprnio.xyz>
2022-06-18 21:25:46 +0300

    Create Label component
    Fix Scrollable

--- web/src/views/Dashboard/Dashboard.styled.ts
+++ web/src/views/Dashboard/Dashboard.styled.ts
@@ -2,8 +2,7 @@ import styled from 'styled-components/macro';
 
 
 export const DashboardContainer = styled.div`
-    height: 95vh;
-    h4{
-        padding: 0 10px;
-    }
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
 `;
--- web/src/views/Dashboard/Dashboard.tsx
+++ web/src/views/Dashboard/Dashboard.tsx
@@ -1,34 +1,23 @@
 import { AssetCard, ProfitCard } from "../../components/Card";
+import { Label } from '../../components/Label/Label';
 import { Scrollable } from "../../components/Scrollable";
 import { DashboardContainer } from "./Dashboard.styled";
 
-
-const profitCardsData = [
-  {amount: 571, label: 'Total earning', timePeriod: 'All time', margin: 21},
-  {amount: 231, label: 'Weekly earning', timePeriod: '7 Days', margin: 53},
-  {amount: -231, label: 'Monthly earning', timePeriod: '7 Days', margin: -53},  
-]
-
-
-const Dashboard = () => 
+const Dashboard = () =>
     <DashboardContainer>
-      <div>
-        <h4>Total earnings</h4>
-        <Scrollable>
-          <ProfitCard label="Total" profit={420} margin={69} />
-          <ProfitCard label="Total" profit={420} margin={69} />
-          <ProfitCard label="Total" profit={420} margin={69} />
-        </Scrollable>
-        <h4>Assets</h4>
-        <Scrollable>
-          <AssetCard label="Bitcoin" profit={420} margin={69} total={1337} />
-          <AssetCard label="Ethereum" profit={420} margin={69} total={1337} />
-          <AssetCard label="Monero" profit={420} margin={69} total={1337} />
-        </Scrollable>
-      </div>
-      <div>
-        
-      </div>
+      <Label type="heading">Dashboard</Label>
+      <Label type="title">Total earnings</Label>
+      <Scrollable>
+        <ProfitCard label="Total" profit={420} margin={69} />
+        <ProfitCard label="Total" profit={420} margin={69} />
+        <ProfitCard label="Total" profit={420} margin={69} />
+      </Scrollable>
+      <Label type="title">Assets</Label>
+      <Scrollable>
+        <AssetCard label="Bitcoin" profit={420} margin={69} total={1337} />
+        <AssetCard label="Ethereum" profit={420} margin={69} total={1337} />
+        <AssetCard label="Monero" profit={420} margin={69} total={1337} />
+      </Scrollable>
     </DashboardContainer>
 ;
 
